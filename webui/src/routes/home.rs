4ddf97eaad1bef1ef37f78bdc09b9972462d3ae2
Eugene Lebedev <duke.tougu@gmail.com>
2023-09-03 11:16:33 +0300

    Add UI layout

--- webui/src/routes/home.rs
+++ webui/src/routes/home.rs
@@ -36,15 +36,118 @@ pub fn HomePage(cx: Scope) -> Element {
 
     cx.render(rsx! {
         div {
-            class: "mb-3",
-            div {
-                format!("PW APP :: UNDER DEVELOPMENT"),
+            nav {
+                class: "navbar bg-dark text-info",
+                div {
+                    class: "container-fluid",
+                    a {
+                        class: "navbar-brand text-light",
+                        href: "#",
+                        "PW"
+                    }
+                }
             },
             div {
-                format!("encrypted: {encrypted}")
-            },
-            div {
-                format!("decrypted: {decrypted}")
+                class: "container bg-white p-5 text-center shadow-sm",
+                div {
+                    class: "text-start",
+                    h4 {
+                        "Message"
+                    }
+                }
+                textarea {
+                    id: "message-input",
+                    class: "form-control",
+                    rows: 5,
+                    autofocus: true,
+                    placeholder: "The data will be encrypted in the browser"
+                },
+                div {
+                    div {
+                        class: "mt-3",
+                        "Secret lifetime:"
+                    },
+                    label {
+                        id: "ttl-one-hour",
+                        class: "mt-2 me-2",
+                        input {
+                            id: "ttl-one-hour",
+                            name: "secret-ttl",
+                            r#type: "radio",
+                            class: "me-1",
+                            checked: true
+                        },
+                        "One hour"
+                    },
+                    label {
+                        id: "ttl-two-hours",
+                        class: "me-2",
+                        input {
+                            id: "ttl-two-hours",
+                            name: "secret-ttl",
+                            r#type: "radio",
+                            class: "me-1"
+                        },
+                        "Two hours"
+                    },
+                    label {
+                        id: "ttl-one-day",
+                        class: "me-2",
+                        input {
+                            id: "ttl-one-day",
+                            name: "secret-ttl",
+                            r#type: "radio",
+                            class: "me-1"
+                        },
+                        "One day"
+                    }
+                },
+
+                div {
+                  class: "mt-3",
+                  label {
+                        id: "one-time-download",
+                        class: "mt-2 me-2",
+                        input {
+                            id: "one-time-download",
+                            name: "one-time-download",
+                            r#type: "checkbox",
+                            class: "me-1"
+                        },
+                        "One time download"
+                    }
+                },
+
+                button {
+                    id: "encrypt-btn",
+                    class: "btn btn-dark mt-5",
+                    disabled: true,
+                    "Encrypt message"
+                },
+
+                div {
+                    class: "footer-links mt-5",
+                    span {
+                        class: "me-1",
+                        "v1.0.0"
+                    },
+                    span {
+                        class: "ms-1 me-1",
+                        "|"
+                    },
+                    a {
+                        class: "me-1 ms-1",
+                        "HOW IT WORKS"
+                    }
+                    span {
+                        class: "ms-1 me-1",
+                        "|"
+                    },
+                    a {
+                        class: "ms-1",
+                        "GITHUB"
+                    }
+                }
             }
         }
     })
