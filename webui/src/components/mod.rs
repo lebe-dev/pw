afff565c4a8e44d2f1f8102e722b7ba4f826824e
Eugene Lebedev <duke.tougu@gmail.com>
2023-09-07 14:56:34 +0300

    webui module: add Notification component

--- webui/src/components/mod.rs
+++ webui/src/components/mod.rs
@@ -1,2 +1,4 @@
 pub mod header;
 pub mod footer;
+
+pub mod notification;
---
+++ webui/src/components/notification.rs
@@ -0,0 +1,46 @@
+use dioxus::prelude::*;
+
+pub enum NotificationType {
+    Loading, Info, Error
+}
+
+#[inline_props]
+pub fn Notification<'a>(cx: Scope, notification_type: NotificationType,
+                        title: &'a str, message: &'a str) -> Element {
+    render! {
+        match notification_type {
+            NotificationType::Loading => {
+                rsx! {
+                    div {
+                        div {
+                            class: "fs-5",
+                            "{title}",
+                        }
+                    }
+                }
+            },
+            NotificationType::Info => {
+                rsx! {
+                    div {
+                        div {
+                            class: "fs-5 mb-2",
+                            "{title}",
+                        },
+                        "{message}"
+                    }
+                }
+            },
+            NotificationType::Error => {
+                rsx! {
+                    div {
+                        div {
+                            class: "fs-5 mb-2 text-danger",
+                            "{title}",
+                        },
+                        "{message}"
+                    }
+                }
+            }
+        }
+    }
+}
