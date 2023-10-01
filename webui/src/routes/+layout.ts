1be9db83d82c63ee33c6988900be98e83dc272bc
Eugene Lebedev <duke.tougu@gmail.com>
2023-10-01 13:48:04 +0300

    webui module: setup static adapter

--- webui/src/routes/+layout.ts
+++ webui/src/routes/+layout.ts
@@ -1,6 +1,8 @@
 import {AppConfig} from "$lib/config";
 import type {LayoutLoad} from './$types';
 
+export const ssr = false;
+
 export const load: LayoutLoad = async () => {
     return {
         config: new AppConfig()
