9e6b97021db9fcb0d4a40097e242fc2879407b4a
Eugene Lebedev <duke.tougu@gmail.com>
2024-05-02 10:24:08 +0300

    Bump app version to 1.2.4

--- webui/src/routes/+page.svelte
+++ webui/src/routes/+page.svelte
@@ -189,7 +189,8 @@
 		{/if}
 
 		<div class="text-gray-400 text-sm">
-			v1.2.3 <span class="ms-1 me-1">|</span> <a href={'https://github.com/lebe-dev/pw/blob/main/docs/faq/FAQ.' + data.config.locale.id + '.md'} target="_blank">{data.config.locale.footerLabels.howItWorks}</a> <span class="ms-1 me-1">|</span> <a href="https://github.com/lebe-dev/pw" target="_blank">GITHUB</a>
+			v1.2.4 <span class="ms-1 me-1">|</span> <a href={'https://github.com/lebe-dev/pw/blob/main/docs/faq/FAQ.' + data.config.locale.id + '.md'}
+			   target="_blank">{data.config.locale.footerLabels.howItWorks}</a> <span class="ms-1 me-1">|</span> <a href="https://github.com/lebe-dev/pw" target="_blank">GITHUB</a>
 		</div>
 	</div>
 
