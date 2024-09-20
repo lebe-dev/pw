e6ba5a02dfc4f9f6d3de931a820c19c53f79ff0d
Eugene Lebedev <duke.tougu@gmail.com>
2024-09-20 23:39:07 +0300

    Migration from ClipboardJS to svelte-copy

--- webui/src/routes/+page.svelte
+++ webui/src/routes/+page.svelte
@@ -5,11 +5,11 @@
 	import {AES} from "crypto-js";
 	import {getEncodedUrlSlug, getUrlBaseHost} from "$lib/url";
 	import {Secret, SecretDownloadPolicy, SecretTTL} from "$lib/secret";
-	import ClipboardJS from "clipboard";
 	import PrecautionMessage from "../components/PrecautionMessage.svelte";
 	import {showError} from "$lib/notifications";
 	import RadioButton from "../components/RadioButton.svelte";
 	import CheckBox from "../components/CheckBox.svelte";
+	import CopyButton from "../components/CopyButton.svelte";
 
 	let inProgress: boolean = true;
 
@@ -28,8 +28,6 @@
 	export let data: PageData;
 
 	onMount(async () => {
-
-		new ClipboardJS('#copy-to-clipboard');
 
 		const response = await fetch('/api/config', {
 			method: 'GET'
@@ -177,16 +175,8 @@
 				<PrecautionMessage message={data.config.locale.homePage.lifetime.oneTimeDownloadPrecautionMessage}/>
 			{/if}
 
-			<div class="mb-9 text-center">
-				<button id="copy-to-clipboard" data-clipboard-target="#secret-url"
-						class="px-3 py-1.5 btn btn-sm btn-neutral hover:btn-accent uppercase rounded disabled:pointer-events-none">
-
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 inline-block">
-						<path stroke-linecap="round" stroke-linejoin="round" d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184" />
-					</svg>
-
-					{data.config.locale.homePage.copyButton}
-				</button>
+			<div class="mb-9 text-center mt-4">
+				<CopyButton data={secretUrl} label={data.config.locale.homePage.copyButton}/>
 			</div>
 
 		{/if}
--- webui/src/routes/s/[id]/+page.svelte
+++ webui/src/routes/s/[id]/+page.svelte
@@ -6,7 +6,7 @@
 	import {SecretDownloadPolicy} from "$lib/secret";
 	import PrecautionMessage from "../../../components/PrecautionMessage.svelte";
 	import {error} from "@sveltejs/kit";
-	import ClipboardJS from "clipboard";
+	import CopyButton from "../../../components/CopyButton.svelte";
 
 	export let data: PageData;
 
@@ -17,7 +17,6 @@
 	let notFound: boolean = false;
 
 	onMount(async () => {
-		new ClipboardJS('#copy-to-clipboard');
 
 		let response = await fetch('/api/config', {
 			method: 'GET'
@@ -104,13 +103,7 @@
 		{:else}
 			<div class="columns-2">
 				<div class="column-xs ps-1">
-					<button id="copy-to-clipboard" data-clipboard-target="#secret-url"
-							class="px-3 py-1.5 btn btn-sm hover:btn-accent uppercase rounded cursor-pointer text-md">
-
-						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 inline-block">
-							<path stroke-linecap="round" stroke-linejoin="round" d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184" />
-						</svg> {data.config.locale.secretUrlPage.copyButton}
-					</button>
+					<CopyButton data={message} label={data.config.locale.secretUrlPage.copyButton}/>
 				</div>
 				<div class="column-xs text-right pe-1">
 					<button class="px-3 py-1.5 ms-3 btn btn-sm hover:bg-red-600 hover:text-primary-content hover:border-red-600 hover:text-white uppercase rounded cursor-pointer text-md"
