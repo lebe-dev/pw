73442f7f7cfc6a55a445db021630c6c4b5e1a8d2
Eugene Lebedev <eugene.0x90@gmail.com>
2025-07-21 14:15:02 +0300

    Code cleanup

--- src/secret/usecase.rs
+++ src/secret/usecase.rs
@@ -42,7 +42,7 @@ pub fn store_secret(
 #[cfg(test)]
 mod tests {
     use crate::secret::model::{SecretDownloadPolicy, SecretTTL};
-    use crate::secret::storage::{DEFAULT_REDIS_CNN_URL, RedisSecretStorage, SecretStorage};
+    use crate::secret::storage::{DEFAULT_REDIS_CNN_URL, RedisSecretStorage};
     use crate::secret::usecase::store_secret;
     use crate::tests::secret::get_sample_secret;
     use crate::tests::string::get_random_string;
