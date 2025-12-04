2237bb242bbd83b98bae7e0d8af423e0a3735f9f
Eugene Lebedev <eugene.0x90@gmail.com>
2025-12-04 23:29:49 +0300

    Bump app version to 1.11.0

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ pub mod integration_tests;
 #[cfg(test)]
 pub mod security_tests;
 
-pub const VERSION: &str = "1.10.3 #1";
+pub const VERSION: &str = "1.11.0 #1";
 
 static INDEX_HTML: &str = "index.html";
 
