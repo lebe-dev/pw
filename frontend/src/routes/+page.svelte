9c202f0b399940f7ce8794eba2457175b3c1ebf1
Eugene Lebedev <eugene.0x90@gmail.com>
2026-01-27 10:05:37 +0300

    Bump app version to 1.14.0

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -397,7 +397,7 @@
 		<a
 			href="https://github.com/lebe-dev/pw/releases"
 			target="_blank"
-			class="hover:text-secondary-foreground hover:dark:text-accent">v1.13.1</a
+			class="hover:text-secondary-foreground hover:dark:text-accent">v1.14.0</a
 		>
 
 		<span class="me-1 ms-1">|</span>
